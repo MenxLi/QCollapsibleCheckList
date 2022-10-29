4a7e5a96105ab108b6ec111f590f3064f1881945
limengxun <limengxun45@outlook.com>
2022-10-29 23:34:10 +0800

    chore: demo

--- demo/demo-cyclic.py
+++ demo/demo-cyclic.py
@@ -3,36 +3,35 @@ from typing import Literal
 from PyQt6.QtWidgets import QApplication
 from QCollapsibleCheckList import CollapsibleCheckList, DataItemAbstract
 
-itemT = Literal["A", "B", "C", "D"]
+itemT = Literal["Root", "A", "B", "C"]
 
-class CycleData(DataItemAbstract):
+class CycleData(str, DataItemAbstract):
    def __init__(self, key: itemT) -> None:
       super().__init__()
-      self.k = key
 
    def isParentOf(self, data: CycleData) -> bool:
-      if self.k == "A":
+      k0 = str(self)
+      k1 = str(data)
+      if k0 == "Root" and k1 == "A":
          return True
-      k0 = self.k
-      k1 = data.k
-      if k0 == "B" and k1 =="C":
+      if k0 == "A" and k1 == "B":
          return True
-      if k0 == "C" and k1 =="D":
+      if k0 == "B" and k1 == "C":
          return True
-      if k0 == "D" and k1 =="B":
+      if k0 == "C" and k1 == "A":
          return True
       return False
    
    def toString(self) -> str:
-      return self.k
+      return str(self)
 
 if __name__ == "__main__":
    app = QApplication([])
    data = [
+      CycleData("Root"),
       CycleData("A"),
       CycleData("B"),
       CycleData("C"),
-      CycleData("D"),
    ]
 
    ccl = CollapsibleCheckList(None, data)
